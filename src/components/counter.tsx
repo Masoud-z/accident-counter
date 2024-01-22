6302e5a928e0aa43fef4eda39060c5c274412b90
Masoud <masoud.zeinalabedini@gmail.com>
2024-01-22 15:15:43 +0330

    add functionality with states

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,16 +1,36 @@
+import { useState } from 'react';
+
 const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [draftCount, setDraftCount] = useState(count);
+
   return (
-    <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
+    <section className="flex w-2/3 flex-col items-center gap-8 border-4 border-primary-500 bg-white p-8 shadow-lg">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
+      <p className="text-6xl">{count}</p>
       <div className="flex gap-2">
-        <button>➖ Decrement</button>
+        <button onClick={() => setCount((count) => count - 1)}>
+          ➖ Decrement
+        </button>
         <button>🔁 Reset</button>
-        <button>➕ Increment</button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          ➕ Increment
+        </button>
       </div>
       <div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="number" value={0} />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setCount(draftCount);
+          }}
+        >
+          <input
+            type="number"
+            id="count-input"
+            name="count-input"
+            value={draftCount}
+            onChange={(e) => setDraftCount(e.target.valueAsNumber)}
+          />
           <button type="submit">Update Counter</button>
         </form>
       </div>
